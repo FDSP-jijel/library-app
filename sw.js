72921cbe54eb06e2576d4ee45c47d7111fb2ab82
FDSP-jijel <boumazanora17@gmail.com>
2026-03-25 11:19:48 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,42 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = "library-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./catalog_FDSP_jijel.csv",
+  "./logo.png",
+  "./faculty.jpg"
+];
+
+// تثبيت Service Worker
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("library-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "catalog_FDSP_jijel.csv",
-        "logo.png",
-        "faculty.jpg"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+// تفعيل Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// تشغيل بدون إنترنت
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
